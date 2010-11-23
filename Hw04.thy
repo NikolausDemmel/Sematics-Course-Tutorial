9d1dfa21bba647db33650d4297965a72b3994398
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2010-11-23 15:31:01 +0100

    Finished HW sheet 4.  * Completed Proofs  * Added Questions for the correctors  * Tidied up a bit.

--- Hw04.thy
+++ Hw04.thy
@@ -21,42 +21,58 @@ where
   t1: "t []"
 | t2: "\<lbrakk>t xs ; t ys\<rbrakk> \<Longrightarrow> t (xs @ [A] @ ys @ [B])"
 
-declare t.intros[simp, intro] (* isn't 'intro' default for these? *)
+(* NOTE: is "\<lbrakk> A1 ; A2 ; ... \<rbrakk> \<Rightarrow> Conclustion" the correct style for inductive defs? *)
+
+declare t.intros[simp, intro] (* NOTE: isn't 'intro' default for these? *)
 declare s.intros[simp, intro]
 
 
 
 section "Part (b)"
 
-(* why cant i swap ys and xs in the assumption for the induct to work? *)
+
+subsection "A lemma in apply style and then isar style"
+
+(* Apply style proof of simple lemma *)
 lemma t_conc: "\<lbrakk> t ys ; t xs \<rbrakk> \<Longrightarrow> t (xs @ ys)"
 apply (induct ys rule: t.induct)
+(* NOTE: why cant i swap ys and xs in the assumption for the induct to work? *)
 apply auto
-by (metis Cons_eq_appendI append_assoc append_self_conv2 t.intros(2))
- 
-(* TODO: show lemma with isar-style proof or proof it inline in the proof below
-lemma t_conc:
-fixes xs ys
-assumes "t xs" "t ys"
-shows "t (xs @ ys)"
-proof (induct ys rule: t.induct)
-  assume "xs = []" 
-  with `t ys` show ?thesis by auto
-next
-  fix x xs' assume "xs = x # xs'"
-sorry
-*)
+apply (metis Cons_eq_appendI append_assoc append_self_conv2 t.intros(2))
+done
 
+(* Same lemma as above, now with isar proof *)
+lemma "\<lbrakk> t ys ; t xs \<rbrakk> \<Longrightarrow> t (xs @ ys)"
+proof (induct ys rule: t.induct)
+  case t1 thus ?case by simp
+next
+  case t2
+  thus ?case by (metis Cons_eq_appendI append_assoc append_self_conv2 t.intros(2))
+qed
+
+(* NOTE: why does this not work?
+lemma
+fixes xs ys
+assumes "t ys" "t xs"
+shows "t (xs @ ys)"
+proof (induct ys rule: t.induct) *)
+
+
+subsection "Now the requested theorem with two proofs"
+
+(* Apply style proof first *)
 theorem "s xs = t xs"
+(* NOTE: How would I proceed if I wanted to show "s = t" without the parameter? *)
 apply auto
 apply (induct xs rule: s.induct)
-apply (rule t.intros(1))
+apply (rule t1)
 apply (metis Cons_eq_append_conv eq_Nil_appendI t.intros)
 apply (simp add: t_conc)
 apply (induct xs rule: t.induct)
 apply auto
 done
 
+(* Same theorem as above, now with isar proof *)
 theorem
 fixes xs
 shows "s xs = t xs"
@@ -64,16 +80,32 @@ proof
   assume "s xs"
   then show "t xs"
   proof (induct rule: s.induct)
-    case s1 thus ?case by simp
+    case s1 thus ?case by (rule t1)
   next
-    case (s2 xs)
+    case s2
     thus ?case by (metis Cons_eq_append_conv eq_Nil_appendI t.intros)
   next
-    case (s3 xs ys)
-    thus ?case by (simp add: t_conc) (* TODO: dont use lemma, add sub proof *)
+    (* ALTERNATIVE 1: use lemma            *)
+    (* case (s3 xs ys)                     *)
+    (* thus ?case by (simp add: t_conc)    *)
+    (* ALTERNATIVE 2: use inline sub-proof *)
+    fix xs ys
+    assume "t ys" "t xs"
+    then show "t (xs @ ys)"
+    (* NOTE: again we have to spell out the case manually to have the "t ys" assumption 
+       appear first. Also including additional assumptions like "s xs" "s ys" also 
+       makes the induct proof fail. I don't quite undestand... *)
+    proof (induct ys rule: t.induct) 
+      case t1 thus ?case by simp
+    next
+      case t2
+      thus ?case by (metis Cons_eq_appendI append_assoc append_self_conv2 t.intros(2))
+    qed
   qed
 next
   assume "t xs"
   then show "s xs"
     by (induct rule: t.induct) auto
 qed
+
+end
