53fd50866eee43d59a3f9e055bf9c86f8b0e6834
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2010-12-03 15:39:29 +0100

    solved ex 06

--- Ex06.thy
+++ Ex06.thy
@@ -1,4 +1,4 @@
-theory Ex06_Template
+theory Ex06
 imports Small_Step
 begin
 
@@ -14,20 +14,40 @@ thm zero_stepsE
 inductive_cases one_stepE[elim!]: "cs \<rightarrow>^(Suc n) cs''"
 thm one_stepE
 
-
 lemma small_steps_n: "cs \<rightarrow>* cs' \<Longrightarrow> (\<exists>n. cs \<rightarrow>^n cs')"
-oops
+proof (induct rule: small_steps.induct)
+qed (blast intro: n_steps.intros)+
+
 
 lemma n_small_steps: "cs \<rightarrow>^n cs' \<Longrightarrow> cs \<rightarrow>* cs'"
-oops
+proof (induct rule: n_steps.induct)
+qed (blast intro: step)+
 
 
 definition
   small_step_equiv :: "com \<Rightarrow> com \<Rightarrow> bool" (infix "\<approx>" 50) where
   "c \<approx> c' == (\<forall>s t n. (c,s) \<rightarrow>^n (SKIP, t)  =  (c', s) \<rightarrow>^n (SKIP, t))"
 
+
 lemma small_eqv_implies_big_eqv: "c \<approx> c' \<Longrightarrow> c \<sim> c'"
-oops
+proof -
+  assume "c \<approx> c'"
+  {
+    fix s t c c'
+    assume "c \<approx> c'" "(c, s) \<Rightarrow> t"
+    then have "(c,s) \<rightarrow>* (SKIP,t)" by (simp add: big_to_small)
+    with small_steps_n  obtain n where "(c,s) \<rightarrow>^n (SKIP, t)" by blast
+    with `c \<approx> c'` small_step_equiv_def have "(c',s) \<rightarrow>^n (SKIP, t)" by auto
+    with n_small_steps have "(c',s) \<rightarrow>* (SKIP, t)" by auto
+    then have "(c', s) \<Rightarrow> t" by (rule small_to_big)
+  }
+  moreover
+  have "(c \<approx> c') = (c' \<approx> c)" by (auto simp add: small_step_equiv_def)
+  ultimately  have 
+    "\<And>s t. \<lbrakk> c \<approx> c' ; (c ,s) \<Rightarrow> t \<rbrakk> \<Longrightarrow> (c',s) \<Rightarrow> t" and 
+    "\<And>s t. \<lbrakk> c \<approx> c' ; (c',s) \<Rightarrow> t \<rbrakk> \<Longrightarrow> (c ,s) \<Rightarrow> t" 
+    by blast+ 
+  with `c \<approx> c'` show "c \<sim> c'" by blast
+qed
 
 end
-
