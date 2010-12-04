7b265ccbbb171d3f394fc59456f087990c1e8df6
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2010-12-04 17:13:17 +0100

    Condensed Hw06 to one file.

--- Hw06.thy
+++ Hw06.thy
@@ -1,27 +1,121 @@
-(* Modified for Homework 06 *)
+header "Semantics homework sheet 6 - Nikolaus Demmel"
 
-header "Small-Step Semantics of Commands"
+theory Hw06 imports BExp begin
 
-theory Hw06Small_Step imports Hw06Big_Step begin
+(* CONTENT FROM Com.thy *)
+
+datatype
+  com = SKIP 
+      | Assign name aexp         ("_ ::= _" [1000, 61] 61)
+      | Semi   com  com          ("_; _"  [60, 61] 60)
+      | If     bexp com com     ("IF _ THEN _ ELSE _"  [0, 0, 61] 61)
+      | While  bexp com         ("WHILE _ DO _"  [0, 61] 61)
+(* BEGIN MODIFIED *)
+      | Or     com com          ("_ OR _" [60, 61] 60)
+(* END MODIFIED *)
+
+
+
+(* CONTENT FROM Big_Step.thy *)
+
+subsection "Big-Step Semantics of Commands"
+
+inductive
+  big_step :: "com * state \<Rightarrow> state \<Rightarrow> bool" (infix "\<Rightarrow>" 55)
+where
+Skip:    "(SKIP,s) \<Rightarrow> s" |
+Assign:  "(x ::= a,s) \<Rightarrow> s(x := aval a s)" |
+Semi:    "\<lbrakk> (c\<^isub>1,s\<^isub>1) \<Rightarrow> s\<^isub>2;  (c\<^isub>2,s\<^isub>2) \<Rightarrow> s\<^isub>3 \<rbrakk> \<Longrightarrow>
+          (c\<^isub>1;c\<^isub>2, s\<^isub>1) \<Rightarrow> s\<^isub>3" |
+
+IfTrue:  "\<lbrakk> bval b s;  (c\<^isub>1,s) \<Rightarrow> t \<rbrakk> \<Longrightarrow>
+         (IF b THEN c\<^isub>1 ELSE c\<^isub>2, s) \<Rightarrow> t" |
+IfFalse: "\<lbrakk> \<not>bval b s;  (c\<^isub>2,s) \<Rightarrow> t \<rbrakk> \<Longrightarrow>
+         (IF b THEN c\<^isub>1 ELSE c\<^isub>2, s) \<Rightarrow> t" |
+
+
+
+WhileFalse: "\<not>bval b s \<Longrightarrow> (WHILE b DO c,s) \<Rightarrow> s" |
+WhileTrue:  "\<lbrakk> bval b s\<^isub>1;  (c,s\<^isub>1) \<Rightarrow> s\<^isub>2;  (WHILE b DO c, s\<^isub>2) \<Rightarrow> s\<^isub>3 \<rbrakk> \<Longrightarrow>
+             (WHILE b DO c, s\<^isub>1) \<Rightarrow> s\<^isub>3" |
+
+(* BEGIN MODIFIED *)
+Or1:     "(c1,s) \<Rightarrow> t \<Longrightarrow> (c1 OR c2, s) \<Rightarrow> t" |
+Or2:     "(c2,s) \<Rightarrow> t \<Longrightarrow> (c1 OR c2, s) \<Rightarrow> t"
+(* END MODIFIED *)
+
+
+text{* We want to execute the big-step rules: *}
+
+code_pred big_step .
+
+definition list :: "state \<Rightarrow> nat \<Rightarrow> nat list" where
+"list s n = map s [0 ..< n]"
+
+inductive exec where
+"(c,nth ns) \<Rightarrow> s  \<Longrightarrow>  exec c ns (list s (length ns))"
+
+code_pred exec .
+
+
+text{* Proof automation: *}
+
+declare big_step.intros [intro]
+
+lemmas big_step_induct = big_step.induct[split_format(complete)]
+
+
+subsection "Rule inversion"
+
+
+inductive_cases skipE[elim!]: "(SKIP,s) \<Rightarrow> t"
+inductive_cases AssignE[elim!]: "(x ::= a,s) \<Rightarrow> t"
+inductive_cases SemiE[elim!]: "(c1;c2,s1) \<Rightarrow> s3"
+inductive_cases IfE[elim!]: "(IF b THEN c1 ELSE c2,s) \<Rightarrow> t"
+inductive_cases WhileE[elim]: "(WHILE b DO c,s) \<Rightarrow> t"
+
+(* BEGIN MODIFIED *)
+inductive_cases OrE[elim!]: "(c1 OR c2,s) \<Rightarrow> t"
+thm OrE
+(* END MODIFIED *)
+
+
+subsection "Command Equivalence"
+
+abbreviation
+  equiv_c :: "com \<Rightarrow> com \<Rightarrow> bool" (infix "\<sim>" 50) where
+  "c \<sim> c' == (\<forall>s t. (c,s) \<Rightarrow> t  =  (c',s) \<Rightarrow> t)"
+
+(* BEGIN MODIFIED *)
+lemma commute_or: "c1 OR c2 \<sim> c2 OR c1" by blast
+(* END MODIFIED *)
+
+subsection "Execution is deterministic"
+
+(* Well... its not any more :-) *)
+
+
+
+(* CONTENT FROM Small_Step.thy *)
 
 subsection "The transition relation"
 
 inductive
   small_step :: "com * state \<Rightarrow> com * state \<Rightarrow> bool" (infix "\<rightarrow>" 55)
 where
-Assign:  "(x ::= a, s) \<rightarrow> (SKIP, s(x := aval a s))" |
+SmallAssign:  "(x ::= a, s) \<rightarrow> (SKIP, s(x := aval a s))" |
 
-Semi1:   "(SKIP;c\<^isub>2,s) \<rightarrow> (c\<^isub>2,s)" |
-Semi2:   "(c\<^isub>1,s) \<rightarrow> (c\<^isub>1',s') \<Longrightarrow> (c\<^isub>1;c\<^isub>2,s) \<rightarrow> (c\<^isub>1';c\<^isub>2,s')" |
+SmallSemi1:   "(SKIP;c\<^isub>2,s) \<rightarrow> (c\<^isub>2,s)" |
+SmallSemi2:   "(c\<^isub>1,s) \<rightarrow> (c\<^isub>1',s') \<Longrightarrow> (c\<^isub>1;c\<^isub>2,s) \<rightarrow> (c\<^isub>1';c\<^isub>2,s')" |
 
-IfTrue:  "bval b s \<Longrightarrow> (IF b THEN c\<^isub>1 ELSE c\<^isub>2,s) \<rightarrow> (c\<^isub>1,s)" |
-IfFalse: "\<not>bval b s \<Longrightarrow> (IF b THEN c\<^isub>1 ELSE c\<^isub>2,s) \<rightarrow> (c\<^isub>2,s)" |
+SmallIfTrue:  "bval b s \<Longrightarrow> (IF b THEN c\<^isub>1 ELSE c\<^isub>2,s) \<rightarrow> (c\<^isub>1,s)" |
+SmallIfFalse: "\<not>bval b s \<Longrightarrow> (IF b THEN c\<^isub>1 ELSE c\<^isub>2,s) \<rightarrow> (c\<^isub>2,s)" |
 
-While:   "(WHILE b DO c,s) \<rightarrow> (IF b THEN c; WHILE b DO c ELSE SKIP,s)" |
+SmallWhile:   "(WHILE b DO c,s) \<rightarrow> (IF b THEN c; WHILE b DO c ELSE SKIP,s)" |
 
 (* BEGIN MODIFIED *)
-Or1:     "(c1 OR c2,s) \<rightarrow> (c1,s)" |
-Or2:     "(c1 OR c2,s) \<rightarrow> (c2,s)"
+SmallOr1:     "(c1 OR c2,s) \<rightarrow> (c1,s)" |
+SmallOr2:     "(c1 OR c2,s) \<rightarrow> (c2,s)"
 (* END MODIFIED *)
 
 inductive
@@ -77,17 +171,14 @@ qed
 
 text{* Rule inversion: *}
 
-inductive_cases SkipE[elim!]: "(SKIP,s) \<rightarrow> ct"
-thm SkipE
-inductive_cases AssignE[elim!]: "(x::=a,s) \<rightarrow> ct"
-thm AssignE
-inductive_cases SemiE[elim]: "(c1;c2,s) \<rightarrow> ct"
-thm SemiE
-inductive_cases IfE[elim!]: "(IF b THEN c1 ELSE c2,s) \<rightarrow> ct"
-inductive_cases WhileE[elim]: "(WHILE b DO c, s) \<rightarrow> ct"
+inductive_cases SmallSkipE[elim!]: "(SKIP,s) \<rightarrow> ct"
+inductive_cases SmallAssignE[elim!]: "(x::=a,s) \<rightarrow> ct"
+inductive_cases SmallSemiE[elim]: "(c1;c2,s) \<rightarrow> ct"
+inductive_cases SmallIfE[elim!]: "(IF b THEN c1 ELSE c2,s) \<rightarrow> ct"
+inductive_cases SmallWhileE[elim]: "(WHILE b DO c, s) \<rightarrow> ct"
 (* BEGIN MODIFIED *)
-inductive_cases OrE[elim!]: "(c1 OR c2, s) \<rightarrow> ct"
-thm OrE
+inductive_cases SmallOrE[elim!]: "(c1 OR c2, s) \<rightarrow> ct"
+thm SmallOrE
 (* END MODIFIED *)
 
 
@@ -98,7 +189,7 @@ proof(induct rule: small_steps_induct)
   case refl thus ?case by simp
 next
   case step
-  thus ?case by (metis Semi2 small_steps.step)
+  thus ?case by (metis SmallSemi2 small_steps.step)
 qed
 
 lemma semi_comp:
@@ -179,11 +270,11 @@ next
   case IfFalse thus ?case by (blast intro: step)
 next
   case WhileFalse thus ?case
-    by (metis step step1 small_step.IfFalse small_step.While)
+    by (metis step step1 SmallIfFalse SmallWhile)
 next
   case WhileTrue
   thus ?case
-    by(metis While semi_comp small_step.IfTrue step[of "(a,b)",standard])
+    by(metis SmallWhile semi_comp SmallIfTrue step[of "(a,b)",standard])
 (* FIXME metis cannot find the proof w/o at least one pair in step *)
 (* BEGIN MODIFIED *)
 next
