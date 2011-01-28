1bbcd447a73c8eb2f29e0345ba6f0d50df67accf
Nikolaus Demmel <nikolaus@nikolaus-demmel.de>
2011-01-28 18:02:46 +0100

    finished ex 12

--- Ex12.thy
+++ Ex12.thy
@@ -1,4 +1,6 @@
-theory Ex12_Template
+header "Semantics exercices and homework sheet 12 - Nikolaus Demmel"
+
+theory Ex12
 imports "../Thys/VC" GCD
 begin
 
@@ -18,25 +20,34 @@ abbreviation Afor :: "name \<Rightarrow> aexp \<Rightarrow> aexp \<Rightarrow> a
   "FOR v FROM a1 TO a2 b DO c \<equiv>
      (v #= a1 ;; WHILE' (Less (V v) a2) b DO (c ;; v #= Plus (V v) (N 1)))"
 
+abbreviation vc_pretaut :: "acom \<Rightarrow> assn \<Rightarrow> state \<Rightarrow> bool" where
+  "vc_pretaut c pc s \<equiv> vc c pc s \<and> pre c pc s"
+
 
 
 section {* Exercise 1 *}
 
 definition MAX :: "acom" where
-  "MAX = SKIP"  (* provide a definition *)
+  "MAX = IF' Less (V 0) (V 1) THEN 2 #= (V 1) ELSE 2 #= (V 0)" 
 
-lemma MAX_sound: "vc MAX (\<lambda>s. s 2 = max (s 0) (s 1)) s"
-  oops  (* provide a proof *)
+lemma MAX_sound: "vc_pretaut MAX (\<lambda>s. s 2 = max (s 0) (s 1)) s"
+unfolding MAX_def
+apply auto
+done
 
 
 
 section {* Exercise 2 *}
 
 definition MULTIPLY :: "acom" where
-  "MULTIPLY = SKIP"  (* provide a definition *)
+  "MULTIPLY = 2 #= (N 0) ;;
+              FOR 3 FROM (N 0) TO (V 0) (\<lambda>s. s 2 = s 3 * s 1 \<and> s 3 \<le> s 0) 
+              DO 2 #= Plus (V 2) (V 1)"
 
-lemma MULTIPLY_sound: "vc MULTIPLY (\<lambda>s. s 2 = s 0 * s 1) s"
-  oops  (* provide a proof *)
+lemma MULTIPLY_sound: "vc_pretaut MULTIPLY (\<lambda>s. s 2 = s 0 * s 1) s"
+unfolding MULTIPLY_def
+apply auto
+done
 
 
 
@@ -46,11 +57,34 @@ fun factorial :: "nat \<Rightarrow> nat" where
   "factorial 0 = 1"
 | "factorial (Suc n) = factorial n * Suc n"
 
-definition FACTORIAL :: "acom" where
-  "FACTORIAL = SKIP"  (* provide a definition *)
+lemma factorial_unfold: 
+  "1 \<le> n \<Longrightarrow> factorial n = n * factorial (n - 1)"
+by (induct n) simp_all
 
-lemma FACTORIAL_sound: "vc FACTORIAL (\<lambda>s. s 1 = factorial (s 0)) s"
-  oops  (* provide a proof *)
+abbreviation InvFAC where 
+  "InvFAC s \<equiv> s 1 = factorial ((s 2) - 1) \<and> 
+              s 2 \<le> (s 0) + 1 \<and> 
+              1 \<le> s 2"
+
+abbreviation InvFAC2 where
+  "InvFAC2 s \<equiv> s 3 = (s 4) * (s 1) \<and> 
+               s 4 \<le> s 2 \<and> 
+               1 \<le> s 2 \<and>
+               s 1 = factorial ((s 2) - 1) \<and> 
+               s 2 \<le> (s 0)"
+
+definition FACTORIAL :: "acom" where
+  "FACTORIAL = 1 #= (N 1) ;;
+               FOR 2 FROM (N 1) TO (Plus (V 0) (N 1)) (\<lambda>s. InvFAC s) DO 
+                ( 3 #= (N 0) ;;
+                  FOR 4 FROM (N 0) TO (V 2) (\<lambda>s. InvFAC2 s)  DO 
+                     3 #= Plus (V 3) (V 1) ;;
+                  1 #= (V 3) )" 
+
+
+
+lemma FACTORIAL_sound: "vc_pretaut FACTORIAL (\<lambda>s. s 1 = factorial (s 0)) s"
+by (auto simp: factorial_unfold FACTORIAL_def)
 
 
 
